093251c17ef9c3d1bb254a72494fc0139f7aec05
MVikhot <Vikhotmykola@Gmail.com>
2019-11-21 20:13:19 +0000

    almoustDone

--- js/index.js
+++ js/index.js
@@ -7,20 +7,6 @@ function openTwitter() {
 function openInstagram() {
     window.open("https://www.instagram.com");
 }
-function search() {
-
-    var name = document.getElementById("searchForm").elements["searchItem"].value;
-    var pattern = name.toLowerCase();
-    var targetId = "";
-
-    var divs = document.getElementsByClassName("col-md-2");
-    for (var i = 0; i < divs.length; i++) {
-        var para = divs[i].getElementsByTagName("p");
-        var index = para[0].innerText.toLowerCase().indexOf(pattern);
-        if (index != -1) {
-            targetId = divs[i].parentNode.id;
-            document.getElementById(targetId).scrollIntoView();
-            break;
-        }
-    }
+function openNews() {
+    window.open("https://mvikhot.github.io/Vi-Tech/news/");
 }
